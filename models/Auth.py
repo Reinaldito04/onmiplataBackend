2714407847067b8285e44822eb94de5971ff5fb5
reinaldito <reybellorin82@gmail.com>
2024-07-06 19:09:00 -0400

    "Added registerInformation model, new API endpoints for user information and deletion, and modified login functionality"

--- models/Auth.py
+++ models/Auth.py
@@ -1,6 +1,11 @@
 from pydantic import BaseModel
 
 
+class registerInformation(BaseModel):
+    username: str
+    description: str 
+
+
 class LoginCredentials(BaseModel):
     username: str
     password: str
--- routers/users.py
+++ routers/users.py
@@ -1,10 +1,54 @@
-from fastapi import APIRouter,HTTPException
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from db.db import create_connection
-from models.Auth import LoginCredentials, RegisterUser
+from models.Auth import LoginCredentials, RegisterUser, registerInformation
 
 
 router = APIRouter()
+
+
+@router.get("/getInformationUsers")
+async def obtenerinformation():
+    conn = create_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Registros")
+    rows = cur.fetchall()
+    result = []
+    for row in rows:
+        result.append({
+            "id": row[0],
+            "description": row[1],
+            "username": row[2]
+        })
+    return result
+
+
+@router.post("/addInformation")
+async def registroInformation(data: registerInformation):
+    conn = create_connection()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO Registros (Descripcion,RealizadoPor) values (?,?)",
+                (data.description, data.username))
+    conn.commit()
+    conn.close()
+    return {
+        "Message": "agregado correctamente"
+    }
+
+
+@router.get("/getUsername")
+async def obtenerUser():
+    conn = create_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT Username,ID FROM users")
+    rows = cur.fetchall()
+    result = []
+    for row in rows:
+        result.append({
+            "username": row[0],
+            "id": row[1]
+        })
+    return result
 
 
 @router.post("/login")
@@ -15,14 +59,25 @@ async def login(credentials: LoginCredentials):
                 (credentials.username, credentials.password))
     user = cur.fetchone()
     conn.close()
-    
+
     if user is None:
-        raise HTTPException(status_code=401, detail="Invalid username or password")
-    
+        raise HTTPException(
+            status_code=401, detail="Invalid username or password")
+
     usuario = user[0]
     tipo = user[2]
-    
+
     return {"message": "Login successful", "username": usuario, "type": tipo}
+
+
+@router.delete("/deleteUser")
+async def delete_user(ID: int):
+    conn = create_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM users WHERE ID =?", (ID,))
+    conn.commit()
+    conn.close()
+    return {"message": "User deleted successfully"}
 
 
 @router.post("/register")
