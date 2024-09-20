2dd65aec41c8f9fded3af3860a4ba75184a39e43
reinaldo <reybellorin82@gmail.com>
2024-09-20 09:38:04 -0400

    "Refactored obtener_datos_contrato_y_pagos function: moved function definition, reformatted code, and updated logic for handling pagos_data"

--- routers/reports.py
+++ routers/reports.py
@@ -85,124 +85,129 @@ def generarReportForYear(year: int = Query(..., description="Año para filtrar l
         conn.close()
         raise HTTPException(status_code=500, detail=str(e))
     
-    def obtener_datos_contrato_y_pagos(id: int):
-        conn = create_connection()
-        cursor = conn.cursor()
+    
+def obtener_datos_contrato_y_pagos(id: int):
+    conn = create_connection()
+    cursor = conn.cursor()
 
-        try:
-            # Obtener datos del contrato
-            cursor.execute("""
-                SELECT 
-                    c.FechaInicio, c.FechaFin, c.Monto, c.Comision, c.FechaPrimerPago, c.DuracionMeses,
-                    cl.Nombre, cl.Apellido, cl.Telefono, cl.Email,
-                    i.Direccion AS DireccionInmueble, 
-                    i.Tipo AS TipoInmueble, 
-                    i.Descripcion AS DescripcionInmueble, 
-                    i.Municipio AS MunicipioInmueble
-                FROM 
-                    Contratos c
-                    JOIN Clientes cl ON c.ClienteID = cl.ID
-                    JOIN Inmuebles i ON c.InmuebleID = i.ID
-                WHERE 
-                    c.ID = ?
-            """, (id,))
-            contrato_data = cursor.fetchone()
-            
-            if contrato_data is None:
-                raise HTTPException(status_code=404, detail="Contrato no encontrado")
-            
-            canon_mensual = contrato_data[2]
-            fecha_primer_pago = datetime.strptime(contrato_data[4], '%Y-%m-%d')  # FechaPrimerPago
-            
-            # Obtener pagos del contrato
-            cursor.execute("""
-                SELECT FechaPago, Monto, Metodo
-                FROM Pagos
-                WHERE ContratoID = ? AND TipoPago = 'Arrendamiento'
-            """, (id,))
-            pagos_data = cursor.fetchall()
-            
-            cursor.execute("""
-                SELECT FechaPago, Monto 
-                FROM Pagos 
-                WHERE ContratoID =? AND TipoPago = 'Deposito De Garantia'
-            """, (id,))
-            garantia_deposito = cursor.fetchall()
-            
-            garantia_sum = sum(garantia[1] for garantia in garantia_deposito)
+    try:
+        # Obtener datos del contrato
+        cursor.execute("""
+            SELECT 
+                c.FechaInicio, c.FechaFin, c.Monto, c.Comision, c.FechaPrimerPago, c.DuracionMeses,
+                cl.Nombre, cl.Apellido, cl.Telefono, cl.Email,
+                i.Direccion AS DireccionInmueble, 
+                i.Tipo AS TipoInmueble, 
+                i.Descripcion AS DescripcionInmueble, 
+                i.Municipio AS MunicipioInmueble
+            FROM 
+                Contratos c
+                JOIN Clientes cl ON c.ClienteID = cl.ID
+                JOIN Inmuebles i ON c.InmuebleID = i.ID
+            WHERE 
+                c.ID = ?
+        """, (id,))
+        contrato_data = cursor.fetchone()
+        
+        if contrato_data is None:
+            raise HTTPException(status_code=404, detail="Contrato no encontrado")
+        
+        canon_mensual = contrato_data[2]
+        fecha_primer_pago = datetime.strptime(contrato_data[4], '%Y-%m-%d')  # FechaPrimerPago
+        
+        # Obtener pagos del contrato
+        cursor.execute("""
+            SELECT FechaPago, Monto, Metodo
+            FROM Pagos
+            WHERE ContratoID = ? AND TipoPago = 'Arrendamiento'
+        """, (id,))
+        pagos_data = cursor.fetchall()
+        
+        cursor.execute("""
+            SELECT FechaPago, Monto 
+            FROM Pagos 
+            WHERE ContratoID =? AND TipoPago = 'Deposito De Garantia'
+        """, (id,))
+        garantia_deposito = cursor.fetchall()
+        
+        garantia_sum = sum(garantia[1] for garantia in garantia_deposito)
 
-            # Lógica de los pagos
-            canones_mensuales = {}
-            depositos_efectuados = []
-            total_depositado = 0
-            fecha_actual = fecha_primer_pago
-            
-            for pago in pagos_data:
-                fecha_pago, monto_pago, metodo_pago = pago
-                mes_anio = fecha_actual.strftime('%B').upper() + "/" + fecha_actual.strftime('%Y')  # Agrupar por mes y año
+        # Lógica de los pagos
+        canones_mensuales = {}
+        depositos_efectuados = []
+        total_depositado = 0
+        fecha_actual = fecha_primer_pago
+        
+        for pago in pagos_data:
+            fecha_pago, monto_pago, metodo_pago = pago
+            mes_anio = fecha_actual.strftime('%B').upper() + "/" + fecha_actual.strftime('%Y')  # Agrupar por mes y año
 
-                while monto_pago >= canon_mensual:
+            while monto_pago > 0:
+                # Si el monto es suficiente para cubrir el canon mensual
+                if monto_pago >= canon_mensual:
                     if mes_anio in canones_mensuales:
                         canones_mensuales[mes_anio] += canon_mensual
                     else:
                         canones_mensuales[mes_anio] = canon_mensual
-                    
                     monto_pago -= canon_mensual
-
-                    # Formatear la fecha de pago al formato Latam
-                    fecha_pago_formateada = datetime.strptime(fecha_pago, '%Y-%m-%d').strftime('%d/%m/%Y')
-                    
-                    if len(depositos_efectuados) == 0 or depositos_efectuados[-1]["Fecha"] != fecha_pago_formateada:
-                        depositos_efectuados.append({
-                            "Fecha": fecha_pago_formateada,  # Fecha formateada
-                            "MODALIDAD": metodo_pago.upper(),
-                            "Cantidad": f"USD {pago[1]:,.2f}"
-                        })
-                        total_depositado += pago[1]
-
-                    # Actualizar el mes siguiente
-                    fecha_actual = fecha_actual.replace(day=1) + timedelta(days=32)
-                    fecha_actual = fecha_actual.replace(day=1)
-                    mes_anio = fecha_actual.strftime('%B').upper() + "/" + fecha_actual.strftime('%Y')  # Actualizar agrupación de mes y año
-                
-                if monto_pago > 0:
+                else:
+                    # Si el monto no es suficiente para cubrir otro mes completo
                     if mes_anio in canones_mensuales:
                         canones_mensuales[mes_anio] += monto_pago
                     else:
                         canones_mensuales[mes_anio] = monto_pago
+                    monto_pago = 0  # El monto se ha distribuido por completo para este mes
 
-            # Convertir canones_mensuales en una lista para retornar el formato adecuado
-            canones_mensuales_list = [{"CANON MES": mes, "Cantidad": f"USD {monto:,.2f}"} for mes, monto in canones_mensuales.items()]
+                # Formatear la fecha de pago al formato Latam
+                fecha_pago_formateada = datetime.strptime(fecha_pago, '%Y-%m-%d').strftime('%d/%m/%Y')
+                
+                if len(depositos_efectuados) == 0 or depositos_efectuados[-1]["Fecha"] != fecha_pago_formateada:
+                    depositos_efectuados.append({
+                        "Fecha": fecha_pago_formateada,  # Fecha formateada
+                        "MODALIDAD": metodo_pago.upper(),
+                        "Cantidad": f"USD {pago[1]:,.2f}"
+                    })
+                    total_depositado += pago[1]
 
-            total_contrato = f"USD {sum(pago[1] for pago in pagos_data):,.2f}"
-            total_depositado_formateado = f"USD {total_depositado:,.2f}"
-            
-            # Formatear las fechas del contrato al formato Latam
-            fecha_inicio_formateada = datetime.strptime(contrato_data[0], '%Y-%m-%d').strftime('%d/%m/%Y')
-            fecha_fin_formateada = datetime.strptime(contrato_data[1], '%Y-%m-%d').strftime('%d/%m/%Y')
-            fecha_contrato = f"{fecha_inicio_formateada} AL {fecha_fin_formateada}"
+                # Avanzar al siguiente mes si hay saldo pendiente
+                if monto_pago > 0:
+                    fecha_actual = fecha_actual.replace(day=1) + timedelta(days=32)
+                    fecha_actual = fecha_actual.replace(day=1)
+                    mes_anio = fecha_actual.strftime('%B').upper() + "/" + fecha_actual.strftime('%Y')
 
-            contrato = ContratoInquilino(
-                INQUILINO=f"{contrato_data[6]} {contrato_data[7]}",
-                N_CONTACTO=contrato_data[8],
-                CORREO=contrato_data[9],
-                INMUEBLE=str(contrato_data[10]),  # Ajusta según tu lógica
-                FECHA_CONTRATO=fecha_contrato,  # Fecha del contrato formateada
-                CANON=str(canon_mensual),
-                DEPOSITO_EN_GARANTIA=str(garantia_sum),
-                CANONES_MENSUALES=canones_mensuales_list,
-                TOTAL_CONTRATO=total_contrato,
-                DEPOSITOS_EFECTUADOS=depositos_efectuados,
-                TOTAL_DEPOSITADO=total_depositado_formateado
-            )
+        # Convertir canones_mensuales en una lista para retornar el formato adecuado
+        canones_mensuales_list = [{"CANON MES": mes, "Cantidad": f"USD {monto:,.2f}"} for mes, monto in canones_mensuales.items()]
 
-            return contrato
+        total_contrato = f"USD {sum(pago[1] for pago in pagos_data):,.2f}"
+        total_depositado_formateado = f"USD {total_depositado:,.2f}"
+        
+        # Formatear las fechas del contrato al formato Latam
+        fecha_inicio_formateada = datetime.strptime(contrato_data[0], '%Y-%m-%d').strftime('%d/%m/%Y')
+        fecha_fin_formateada = datetime.strptime(contrato_data[1], '%Y-%m-%d').strftime('%d/%m/%Y')
+        fecha_contrato = f"{fecha_inicio_formateada} AL {fecha_fin_formateada}"
 
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Error al consultar la base de datos: {str(e)}")
-        finally:
-            if conn:
-                conn.close()
+        contrato = ContratoInquilino(
+            INQUILINO=f"{contrato_data[6]} {contrato_data[7]}",
+            N_CONTACTO=contrato_data[8],
+            CORREO=contrato_data[9],
+            INMUEBLE=str(contrato_data[10]),  # Ajusta según tu lógica
+            FECHA_CONTRATO=fecha_contrato,  # Fecha del contrato formateada
+            CANON=str(canon_mensual),
+            DEPOSITO_EN_GARANTIA=str(garantia_sum),
+            CANONES_MENSUALES=canones_mensuales_list,
+            TOTAL_CONTRATO=total_contrato,
+            DEPOSITOS_EFECTUADOS=depositos_efectuados,
+            TOTAL_DEPOSITADO=total_depositado_formateado
+        )
+
+        return contrato
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al consultar la base de datos: {str(e)}")
+    finally:
+        if conn:
+            conn.close()
+
 
         
 @router.post('/report-pays/{id}')
